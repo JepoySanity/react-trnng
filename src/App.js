6f364dceb78d508e8bb21a3c34b818361b210313
jepoy <factoran20@gmail.com>
2022-09-12 14:27:20 +0800

    added Members component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Members from "./Components/Members"
 import MemberNew from "./Components/Members/NewMember"
 import MemberEdit from "./Components/Members/EditMember"
 import MemberInfo from "./Components/Members/MemberInfo"
+import Container from "@mui/material/Container"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Login/> } />
-          <Route path="/members" element={ <Members/> } />
-          <Route path="/member/new" element={ <MemberNew/> } />
-          <Route path="/member/edit/:member_id" element={ <MemberEdit/> } />
-          <Route path="/member/info/:member_id" element={ <MemberInfo/> } />
-        </Routes>
-      </BrowserRouter>
+      <Container maxWidth="lg" sx={{ mt:8 }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Login/> } />
+            <Route path="/members" element={ <Members/> } />
+            <Route path="/member/new" element={ <MemberNew/> } />
+            <Route path="/member/edit/:member_id" element={ <MemberEdit/> } />
+            <Route path="/member/info/:member_id" element={ <MemberInfo/> } />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
--- src/Components/Members/index.jsx
+++ src/Components/Members/index.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+
+function createData(name, status, email, department, location) {
+  return { name, status, email, department, location };
+}
+
+const rows = [
+  createData('John Jefferson Factoran', 'active', 'john.factoran@awsys-i.com', 'Dev F', 'Alabang'),
+  createData('Christian Gil Aquino', 'active', 'christian.aquino@awsys-i.com', 'Dev F', 'Makati'),
+];
 
 export default function index() {
   return (
-    <div>List of Members</div>
-  )
+    <>
+      <Typography sx={{ mb:4 }} variant="h4">List of Members</Typography>
+      <Button variant="contained" component={Link} to="/member/new" sx={{ mb:4 }}>New</Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Department</TableCell>
+              <TableCell align="right">Location</TableCell>
+              <TableCell align="right">Modify</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.status}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.department}</TableCell>
+                <TableCell align="right">{row.location}</TableCell>
+                <TableCell align="right">
+                  <Button variant='outlined'>
+                    edit
+                  </Button>
+                  &ensp;
+                  <Button variant='outlined' color='error'>
+                    delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
