b5932c0a6c1e27d3328b46f6124baf67a72f621b
jepoy <factoran20@gmail.com>
2022-09-12 16:47:05 +0800

    added create member component

--- src/Components/Members/index.jsx
+++ src/Components/Members/index.jsx
@@ -8,7 +8,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 function createData(name, status, email, department, location) {
   return { name, status, email, department, location };
@@ -22,47 +23,49 @@ const rows = [
 export default function index() {
   return (
     <>
-      <Typography sx={{ mb:4 }} variant="h4">List of Members</Typography>
-      <Button variant="contained" component={Link} to="/member/new" sx={{ mb:4 }}>New</Button>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">Email</TableCell>
-              <TableCell align="right">Department</TableCell>
-              <TableCell align="right">Location</TableCell>
-              <TableCell align="right">Modify</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.status}</TableCell>
-                <TableCell align="right">{row.email}</TableCell>
-                <TableCell align="right">{row.department}</TableCell>
-                <TableCell align="right">{row.location}</TableCell>
-                <TableCell align="right">
-                  <Button variant='outlined'>
-                    edit
-                  </Button>
-                  &ensp;
-                  <Button variant='outlined' color='error'>
-                    delete
-                  </Button>
-                </TableCell>
+      <Grid component={Paper} sx={{ p:4 }}>
+        <Typography sx={{ mb:4 }} variant="h5">LIST OF MEMBERS</Typography>
+        <Button sx={{ mb:4 }} variant="contained" component={Link} to="/member/new"><PersonAddAltIcon/>&ensp;New</Button>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Department</TableCell>
+                <TableCell align="right">Location</TableCell>
+                <TableCell align="right">Modify</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.status}</TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">{row.department}</TableCell>
+                  <TableCell align="right">{row.location}</TableCell>
+                  <TableCell align="right">
+                    <Button variant='outlined'>
+                      edit
+                    </Button>
+                    &ensp;
+                    <Button variant='outlined' color='error'>
+                      delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
     </>
   );
 }
