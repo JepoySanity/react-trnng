43458aa2c541d1aba0db8e9e9c6af460d8e24bc5
Jepoy <factoran20@gmail.com>
2022-09-27 10:47:52 +0800

    change other inputs to select input respectively

--- src/Components/Members/EditMember.jsx
+++ src/Components/Members/EditMember.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link, useParams } from "react-router-dom"
-import { Typography, TextField, Button, Grid, Paper } from "@mui/material";
+import { Typography, TextField, Button, Grid, Paper, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import { useFormik } from "formik";
 import { toast } from 'react-toastify';
 import axios from "axios";
@@ -54,16 +54,13 @@ export default function NewMember() {
         .max(30,"name must not exceed 30 characters")
         .required("required"),
       status: Yup.string()
-        .max(8,"name must not exceed 8 characters")
         .required("required"),
       email: Yup.string()
         .email("must be a valid email")
         .required("required"),
       department: Yup.string()
-        .max(6,"name must not exceed 6 characters")
         .required("required"),
       location: Yup.string()
-        .max(15,"name must not exceed 15 characters")
         .required("required"),
     }),
 
@@ -127,16 +124,28 @@ export default function NewMember() {
             />
             {formik.touched.name && formik.errors.name ? <p style={{color: 'red', marginLeft: '5px', marginTop: '-2px', marginBottom: '-8px'}}>{formik.errors.name}</p> : null}
             <br />
-            <TextField
-              style={{ width: "500px", margin: "5px" }}
-              type="text"
-              label="Status"
-              variant="outlined"
-              name="status"
-              value={formik.values.status}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
+            <FormControl sx={{ width: "500px", margin: "5px" }}>
+              <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
+              <Select
+                name="status"
+                label="Status"
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={formik.values.status}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="Active">
+                  Active
+                </MenuItem>
+                <MenuItem value="Inactive">
+                  Inactive
+                </MenuItem>
+              </Select>
+            </FormControl>
             {formik.touched.status && formik.errors.status ? <p style={{color: 'red', marginLeft: '5px', marginTop: '-2px', marginBottom: '-8px'}}>{formik.errors.status}</p> : null}
             <br />
             <TextField
@@ -151,28 +160,94 @@ export default function NewMember() {
             />
             {formik.touched.email && formik.errors.email ? <p style={{color: 'red', marginLeft: '5px', marginTop: '-2px', marginBottom: '-8px'}}>{formik.errors.email}</p> : null}
             <br />
-            <TextField
-              style={{ width: "500px", margin: "5px" }}
-              type="text"
-              label="Department"
-              variant="outlined"
-              name="department"
-              value={formik.values.department}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
+            <FormControl sx={{ width: "500px", margin: "5px" }}>
+              <InputLabel id="demo-simple-select-helper-label">Department</InputLabel>
+              <Select
+                name="department"
+                label="Department"
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={formik.values.department}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="Dev 2">
+                  Dev 2
+                </MenuItem>
+                <MenuItem value="Dev 3">
+                  Dev 3
+                </MenuItem>
+                <MenuItem value="Dev 5">
+                  Dev 5
+                </MenuItem>
+                <MenuItem value="Dev A">
+                  Dev A
+                </MenuItem>
+                <MenuItem value="Dev B">
+                  Dev B
+                </MenuItem>
+                <MenuItem value="Dev C">
+                  Dev C
+                </MenuItem>
+                <MenuItem value="Dev D">
+                  Dev D
+                </MenuItem>
+                <MenuItem value="Dev E">
+                  Dev E
+                </MenuItem>
+                <MenuItem value="Dev F">
+                  Dev F
+                </MenuItem>
+                <MenuItem value="Dev G">
+                  Dev G
+                </MenuItem>
+                <MenuItem value="Dev H">
+                  Dev H
+                </MenuItem>
+                <MenuItem value="Dev I">
+                  Dev I
+                </MenuItem>
+                <MenuItem value="Dev J">
+                  Dev J
+                </MenuItem>
+                <MenuItem value="Dev K">
+                  Dev K
+                </MenuItem>
+                <MenuItem value="Dev L">
+                  Dev L
+                </MenuItem>
+                <MenuItem value="Dev M">
+                  Dev M
+                </MenuItem>
+              </Select>
+            </FormControl>
             {formik.touched.department && formik.errors.department ? <p style={{color: 'red', marginLeft: '5px', marginTop: '-2px', marginBottom: '-8px'}}>{formik.errors.department}</p> : null}
             <br />
-            <TextField
-              style={{ width: "500px", margin: "5px" }}
-              type="text"
-              label="Location"
-              variant="outlined"
-              name="location"
-              value={formik.values.location}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
+            <FormControl sx={{ width: "500px", margin: "5px" }}>
+              <InputLabel id="demo-simple-select-helper-label">Location</InputLabel>
+              <Select
+                name="location"
+                label="Location"
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+                value={formik.values.location}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              >
+                <MenuItem value="Alabang">
+                  Alabang
+                </MenuItem>
+                <MenuItem value="Cebu">
+                  Cebu
+                </MenuItem>
+                <MenuItem value="Makati">
+                  Makati
+                </MenuItem>
+              </Select>
+            </FormControl>
             {formik.touched.location && formik.errors.location ? <p style={{color: 'red', marginLeft: '5px', marginTop: '-2px', marginBottom: '-8px'}}>{formik.errors.location}</p> : null}
             <br />
             <Grid   
